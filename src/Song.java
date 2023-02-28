2623322f340b1f6d4e8f4847f698c8b6b51a52cd
vanessaw <vanessaw2025@student.cis.edu.hk>
2023-02-28 23:33:48 +0800

    Added Song class.

--- src/Song.java
+++ src/Song.java
@@ -1,4 +1,36 @@
+import java.util.*;
 public class Song {
 
-    private String
-}
+    private String title;
+    private String artist;
+
+    private ArrayList<String> nameList = new ArrayList();
+
+    public Song(String t, String a){
+        title = t;
+        artist = a;
+    }
+
+    public String getTitle(){ return title;}
+
+    public String getArtist(){ return artist;}
+
+    public int howMany(ArrayList<String> arrList) {
+        arrList.replaceAll(String::toLowerCase);
+        nameList.addAll(arrList);
+        //int newNum = nameList.size();
+        int num = arrList.size();
+
+        for (int i = nameList.size() - arrList.size(); i < nameList.size(); i++) {
+            for (int j = 0; j < nameList.size() - arrList.size(); j++) {
+                if (nameList.get(i).equals(nameList.get(j))) {
+                    num--;
+                }
+            }
+        }
+        if(Collections.frequency(arrList,arrList.get(0))==arrList.size() && num == arrList.size()){
+            num = 1;
+        }
+        return num;
+    }
+    }
